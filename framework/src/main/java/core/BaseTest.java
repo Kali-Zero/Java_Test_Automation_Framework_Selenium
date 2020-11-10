9e8010d579cb0f7ff314b351e165e5b7dcc52e07
Kali <calista.sullivan@gmail.com>
2020-11-09 18:50:12 -0800

    fixing part of MSEdge issues

--- framework/src/main/java/core/BaseTest.java
+++ framework/src/main/java/core/BaseTest.java
@@ -4,6 +4,7 @@ import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.EdgeDriverManager;
 import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
@@ -14,20 +15,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import static java.lang.System.getProperty;
+import static java.lang.System.setProperty;
 
 public class BaseTest {
 
@@ -35,6 +40,7 @@ public class BaseTest {
     public WebDriver driver;
     public ExtentTest test;
     public Properties prop = new Properties();
+    public Properties browserProps = new Properties();
     private static ExtentReports extent;
     private final Dimension d = new Dimension(1280,960);
     private final String extentFolder = "./../ExtentReports/";
@@ -57,6 +63,7 @@ public class BaseTest {
         FileUtils.copyFile(source, new File(extentFolder+reportTitle()+"/"+reportTitle()+" - Screenshots/"+screenshotName+" - "+timeStamp+".png"));
         return destination;
     }
+
     public void login(){
         basePage.usernameField(driver).sendKeys(prop.getProperty("base_username"));
         basePage.passwordField(driver).sendKeys(prop.getProperty("base_password"));
@@ -70,31 +77,50 @@ public class BaseTest {
     }
 
     @BeforeMethod
-    public FirefoxDriver beforeMethod(Method method) throws IOException {
+    public void beforeMethod(Method method) throws IOException {
+
+        //For Grid Functionality
+        //threadDriver = new ThreadLocal<RemoteWebDriver>();
+        //DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+        browserProps.load(new FileInputStream(new File("./../framework/src/main/java/resources/browser.properties")));
         prop.load(new FileInputStream(new File("./../framework/src/main/java/resources/config.properties")));
-
-        //===This will display the method name as the test title
-        test = extent.startTest((this.getClass().getSimpleName()+" - "+method.getName()));
-
-        //===This will display the method description as the test title (Not well formatted)
-        //test = extent.startTest((this.getClass().getSimpleName()+" - "+method.getAnnotation(Test.class).description()));
-
-        switch (prop.getProperty("browser")) {
+        test = extent.startTest((this.getClass().getSimpleName() + " - " + method.getName()));
+        switch (browserProps.getProperty("browser")) {
             case "Chrome":
-                WebDriverManager.chromedriver().version(prop.getProperty("chrome_version")).setup();
-                System.setProperty("is_headless", prop.getProperty("is_headless"));
-                String chrome_headless = System.getProperty("is_headless");
+
+                //TODO=============================Fix the download folder==============================================
+                //Downloads the file... but puts it in the wrong place: \regressiontests\TestDownloadFolder
+                //System.getProperty("user.dir") +File.separator +"TestDownloadFolder";
+
+                //Puts the folder in the right place, but doesn't put the file in it.
+                //System.getProperty("user.dir") +extentFolder +reportTitle() +"/TestDownloads";
+
+                //Current test file download location:
+                String testDownloadFolder = getProperty("user.dir")
+                        +"."+File.separator+".."+File.separator
+                        +"ExtentReports"+File.separator+reportTitle() +File.separator
+                        +"TestDownloadFolder";
+                //TODO==================================================================================================
+
+                WebDriverManager.chromedriver().version(browserProps.getProperty("chromedriver_version")).setup();
+                setProperty("is_headless", browserProps.getProperty("is_headless"));
+                String chrome_headless = getProperty("is_headless");
                 ChromeDriverManager.chromedriver();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                Map<String, Object> prefs = new HashMap<>();
+                chromeOptions.setExperimentalOption("prefs", prefs);
+                prefs.put("profile.default_content_settings.popups", 0);
+                prefs.put("download.default_directory",testDownloadFolder);
                 if ("true".equals(chrome_headless)) {
-                    ChromeOptions chromeOptions = new ChromeOptions();
-                    chromeOptions.addArguments("--headless");
+                    chromeOptions.addArguments("--headless", "--no-sandbox");
                     driver = new ChromeDriver(chromeOptions);
                 } else {driver = new ChromeDriver();}
                 break;
             case "Firefox":
-                WebDriverManager.firefoxdriver().version(prop.getProperty("firefox_version")).setup();
-                System.setProperty("is_headless", prop.getProperty("is_headless"));
-                String firefox_headless = System.getProperty("is_headless");
+                WebDriverManager.firefoxdriver().version(browserProps.getProperty("firefox_version")).setup();
+                setProperty("is_headless", browserProps.getProperty("is_headless"));
+                String firefox_headless = getProperty("is_headless");
                 FirefoxDriverManager.firefoxdriver();
                 if ("true".equals(firefox_headless)) {
                     FirefoxOptions firefoxOptions = new FirefoxOptions();
@@ -102,21 +128,34 @@ public class BaseTest {
                     driver = new FirefoxDriver(firefoxOptions);
                 } else {driver = new FirefoxDriver();}
                 break;
-            case "MSEdge":  //TODO: Currently broken - fix later!!!
-                WebDriverManager.edgedriver().version(prop.getProperty("edge_version")).setup();
-                driver = new EdgeDriver();
-                break;
-            case "IE11":
-                WebDriverManager.iedriver().version(prop.getProperty("ie_version")).setup();
+            case "IE11":  //TODO: SUPER SLOW for some reason - Investigate later.
+                WebDriverManager.iedriver().version(browserProps.getProperty("ie_driver_version")).setup();
                 driver = new InternetExplorerDriver();
                 break;
+            case "MSEdge":
+                WebDriverManager.edgedriver().version(browserProps.getProperty("ms_edge_version")).setup();
+                setProperty("is_headless", browserProps.getProperty("is_headless"));
+                String msEdge_headless = getProperty("is_headless");
+                EdgeDriverManager.edgedriver();
+                EdgeOptions edgeOptions = new EdgeOptions();
+
+                //TODO: Why won't it run headless?!
+                if ("true".equals(msEdge_headless)) {
+                    //edgeOptions.addArguments("--headless", true);
+                    edgeOptions.setCapability("--headless", true);
+                    driver = new EdgeDriver(edgeOptions);
+                } else {driver = new EdgeDriver();}
+                break;
             default:
-                throw new IllegalStateException("Unexpected Browser Type: " + prop.getProperty("browser"));
+                throw new IllegalStateException("Unexpected Browser Type: " + browserProps.getProperty("browser"));
         }
         driver.manage().window().setSize(d);
-        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.get(prop.getProperty("base_url"));
-        return null;
+
+        //For Grid Functionality
+        //threadDriver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), desiredCapabilities));
+        //threadDriver.set(new RemoteWebDriver(new URL(prop.getProperty("base_url")), desiredCapabilities));
     }
 
     @AfterMethod
